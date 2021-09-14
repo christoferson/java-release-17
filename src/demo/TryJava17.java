1019eb392fc0ff9ed70a1e81db02a4f150d72bf1
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-09-15 00:05:14 +0900

    Add PatternMatchingSwitchEx2

--- src/demo/TryJava17.java
+++ src/demo/TryJava17.java
@@ -4,6 +4,9 @@ public class TryJava17 {
 
 	public static void main(String[] args) {
 		PatternMatchingSwitchEx1("bar");
+		PatternMatchingSwitchEx2("longstring");
+		PatternMatchingSwitchEx2("str");
+		PatternMatchingSwitchEx2(new Object());
 	}
 
 	private static void PatternMatchingSwitchEx1(Object o) {
@@ -13,6 +16,16 @@ public class TryJava17 {
 			case String s  -> System.out.println("Received String!");
 			default		   -> System.out.println("Received Object!");
 		}
+	}
+	
+	private static void PatternMatchingSwitchEx2(Object o) {
+	   switch (o) {
+	     case Integer i: System.out.println("Integer: " + i); break;
+	     case String s && s.length() > 5: System.out.println("String > 5: " + s); break;
+	     case String s: System.out.println("String: " + s); break;
+	     case TryJava17 t: System.out.println("TryJava17: " + t); break;
+	     default : System.out.println("Object: " + o);
+	   }
 	}
 
 }
