bbd27ffce75f6cf08b93e2004df980457d77ccdc
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-09-16 01:30:33 +0900

    Add PatternMatchingSwitchEx4

--- src/demo/TryJava17.java
+++ src/demo/TryJava17.java
@@ -12,6 +12,9 @@ public class TryJava17 {
 		PatternMatchingSwitchEx3("longstring");
 		PatternMatchingSwitchEx3("str");
 		PatternMatchingSwitchEx3(new Object());
+		System.out.println("Try Pattern Matching Switch Ex4");
+		PatternMatchingSwitchEx4(Integer.valueOf(12));
+		PatternMatchingSwitchEx4(Integer.valueOf(7));
 	}
 
 	private static void PatternMatchingSwitchEx1(Object o) {
@@ -40,7 +43,7 @@ public class TryJava17 {
 	       System.out.println(I); 
 	       break; 
 	     case String s && s.length() > 5: 
-	       System.out.println("String s && s.length()>5"); 
+	       System.out.println("String s && s.length() > 5"); 
 	       System.out.println(s); 
 	       break;
 	     case String s: 
@@ -48,12 +51,24 @@ public class TryJava17 {
 	       System.out.println(s);
 	       break; 
 	     case TryJava17 t:
-	       System.out.println("X"); 
+	       System.out.println("TryJava17"); 
 	       System.out.println(t);
 	       break;
 	     default : System.out.println("Object"); 
 	   }
 	}
 		
-
+	private static void PatternMatchingSwitchEx4(Object o) {
+	   switch (o) {
+		   case Integer i && i > 10:
+		     System.out.println("Input Value Greater than 10: " + o);
+		     break;
+		   case Integer j && j > 5:
+		     System.out.println("Input Value Greater than 5: " + o);
+		     break;
+		   default:
+		     System.out.println("Object: " + o);
+	   }
+	}
+	
 }
