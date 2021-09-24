96e383a67f9725fbf5f07cd92ed6a48472c12edb
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-09-24 14:29:30 +0900

    Add StrictFpEx1

---
+++ src/demo/TryJava17StrictFp.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class TryJava17StrictFp {
+
+	public static void main(String[] args) {
+		StrictFpEx1 ex1 = new StrictFpEx1();
+
+	}
+	
+	static strictfp public class StrictFpEx1 { // strictfp applies to all methods in this class
+	}
+
+}
