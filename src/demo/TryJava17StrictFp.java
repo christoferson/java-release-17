6319d4a7105c3ed03b3e5d83dd8a0e8e495d227a
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-09-25 01:00:03 +0900

    Add StrictFpEx2

--- src/demo/TryJava17StrictFp.java
+++ src/demo/TryJava17StrictFp.java
@@ -4,10 +4,15 @@ public class TryJava17StrictFp {
 
 	public static void main(String[] args) {
 		StrictFpEx1 ex1 = new StrictFpEx1();
+		
+		StrictFpEx2 ex2 = null;
 
 	}
 	
 	static strictfp public class StrictFpEx1 { // strictfp applies to all methods in this class
+	}
+	
+	strictfp interface StrictFpEx2 {// strictfp applies to all methods in this interface
 	}
 
 }
