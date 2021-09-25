7a3a6127305e0af132426c2064177adacd97b8b4
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-09-26 00:07:57 +0900

    Add StrictFpEx4

--- src/demo/TryJava17StrictFp.java
+++ src/demo/TryJava17StrictFp.java
@@ -7,7 +7,9 @@ public class TryJava17StrictFp {
 		
 		StrictFpEx2 ex2 = null;
 		
-		StrictFpEx3 ex3 = new StrictFpEx3();
+		StrictFpEx3 ex3 = new StrictFpEx3(); ex3.foo();
+		
+		StrictFpEx4 ex4 = new StrictFpEx4(); ex4.foo();
 
 	}
 	
@@ -19,6 +21,15 @@ public class TryJava17StrictFp {
 	
 	static class StrictFpEx3 {
 		strictfp void foo() {// strictfp applies to method foo only
+			var dec = 187.56 / 12.5;
+			System.out.println(dec);
+		}
+	}
+	
+	static strictfp class StrictFpEx4 {// strictfp applies to all methods in this class
+		strictfp void foo() {// strictfp declaration redundant, but still valid
+			var dec = 187.56 / 12.5;
+			System.out.println(dec);			
 		}
 	}
 
